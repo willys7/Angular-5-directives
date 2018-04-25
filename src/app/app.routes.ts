4cd7c72e9dda503484b8b584f7a24fb3ce33233d
willys7 <wilfuentes0320@gmail.com>
2018-04-25 15:57:25 -0600

    child routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import {RouterModule, Routes} from '@angular/router'
-import {HomeComponent} from './components/home/home.component'
-import {UserComponent} from './components/user/user.component'
+import {HomeComponent} from './components/home/home.component';
+import {UserComponent} from './components/user/user.component';
+import {UserDetailsComponent} from './components/user/user-details.component';
+import {UserEditComponent} from './components/user/user-edit.component';
+import {UserNewComponent} from './components/user/user-new.component';
 
 const APP_ROUTES: Routes = [
     {path: 'home', component: HomeComponent},
-    {path: 'user', component: UserComponent},
+    {
+      path: 'user/:id', 
+      component: UserComponent,
+      children: [
+        {path: 'new', component: UserNewComponent},
+        {path: 'edit', component: UserEditComponent},
+        {path: 'details', component: UserDetailsComponent},
+        {path: '**', pathMatch: 'full', redirectTo: 'new'}
+      ]
+    },
     {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
