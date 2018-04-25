223b864c208eb53e2916f4106b87967e960c0f0f
willys7 <wilfuentes0320@gmail.com>
2018-04-24 19:34:53 -0600

    style directive

--- src/app/components/ng-style/ng-style.component.ts
+++ src/app/components/ng-style/ng-style.component.ts
@@ -3,15 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ng-style',
   template: `
-    <p [ngStyle]="{'font-size': size+'px'}">
+    <p [style.fontSize.px]="size">
       Hola mundo... esta es una etiqueta
     </p>
+    <button class="btn btn-primary" (click)="size = size +5"><i class="fa fa-plus"></i></button>
+    <button class="btn btn-primary" (click)="size =size -5"><i class="fa fa-minus"></i></button>
   `,
   styles: []
 })
 export class NgStyleComponent implements OnInit {
 
-  size:number = 20;
+  size:number = 50;
   constructor() { }
 
   ngOnInit() {
